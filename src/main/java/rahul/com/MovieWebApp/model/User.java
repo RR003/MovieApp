2d9cbcc3a9a6e898e29dc080994d013b1809b3db
RR003 <mail2rahulraja@gmail.com>
2021-06-09 10:47:56 -0400

    changed back to mysql db

--- src/main/java/rahul/com/MovieWebApp/model/User.java
+++ src/main/java/rahul/com/MovieWebApp/model/User.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Optional;
 
 @Entity
-@Table(name = "user_info")
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
